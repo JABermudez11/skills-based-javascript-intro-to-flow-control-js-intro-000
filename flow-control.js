b60141b493011a25bff085333867938c3ea0d9e8
Justin Bermudez <justinanbermudez@gmail.com>
2019-12-06 10:12:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,34 @@
 function basicTeenager(age) {
-
+  if(age < 20 || age > 12){
+    return('You are a teenager!');
+  } else {
+    return('You are not a teenager');
+  }
 }
 
 function teenager(age) {
-
+  if(age < 20 || age > 12){
+    return('You are a teenager!');
+  } else {
+    return('You are not a teenager');
+  }
 }
 
 function ageChecker(age) {
-
+  if(age < 12){
+    return('You are a kid');
+  } else if (age > 19) {
+    return('You are a grownup');
+  } else {
+    return('You are a teenager!');
+  }
 }
 
 function ternaryTeenager(age) {
-
+  age ? (age < 20 && age > 12) {
+    return('You are a teenager!');
+  }
+  return('You are not a teenager');
 }
 
 function switchAge(age) {
